6198f3ee19caa7ac0c81a6f1656fa8d615d3a857
Ezizmuhammet <152854769+EzizmuhammetAshyrmuhammedow@users.noreply.github.com>
2024-03-05 13:24:43 +0000

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,6 @@
+var menuBtn = document.getElementById('menuButton')
+var menu = document.getElementById('menu')
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+  });
